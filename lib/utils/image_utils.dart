4def8fbaa90e1d33b793c78136e30428e898ace2
MrDesilvestri <124096886+MrDesilvestri@users.noreply.github.com>
2025-05-26 20:20:33 -0500

    Update image_utils.dart

--- lib/utils/image_utils.dart
+++ lib/utils/image_utils.dart
@@ -6,22 +6,18 @@ String getFullImageUrl(String? path, {required String placeholder}) {
 
     path = path.replaceAll('"', '').trim();
 
-    // ← Caso especial: strapi plato vacío → forzar asset local
     if (path.contains('/assets/recipes/platovacio')) {
       return placeholder;
     }
 
-    // ← Si ya es url completa
     if (path.startsWith('http')) return path;
 
-    // ← Si es un asset interno (ej: assets/chefs/default_user.png)
     final assetReg =
         RegExp(r'^/?assets/.*\.(png|jpe?g|webp)$', caseSensitive: false);
     if (assetReg.hasMatch(path)) {
       return path.startsWith('/') ? path.substring(1) : path;
     }
 
-    // ← Si es relativo de Strapi
     final base = dotenv.env['STRAPI_URL']?.replaceAll(RegExp(r'/$'), '') ?? '';
     final fixedPath = path.startsWith('/') ? path : '/$path';
     return '$base$fixedPath';
